0468ef38b4c1d1fd8df14578bfad35455f1c46b4
ldaa99-simulacroexamen-oexvp029 <ldaa99-simulacroexamen-oexvp029@local>
2018-05-24 13:55:19 +0000

    simulacro

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,9 @@
 #Escriba el codigo necesario para que al ejecutar
 #python ejercicio1.py se impriman los enteros del 10 al 1 en cuenta regresiva.
+
+a=10
+while a>0:
+    print(a)
+    a-=1
+    
+    
